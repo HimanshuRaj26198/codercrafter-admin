f0e1c3933542ee3587e78bb907a71b25268e36fc
himanshu26198 <himanshu26198@gmail.com>
2025-03-01 17:37:11 +0530

    added random key logic

--- src/app/_components/CMSForm/page.js
+++ src/app/_components/CMSForm/page.js
@@ -352,7 +352,7 @@ const CMSForm = () => {
             const colRef = collection(db, "posts");
             let data = JSON.parse(localStorage.getItem("current_post_article"));
             const slug = createSlug(postTitle);
-            const newDoc = await addDoc(colRef, { ...data, createdAt: Timestamp.now(), slug: slug });
+            const newDoc = await addDoc(colRef, { ...data, createdAt: Timestamp.now(), slug: slug, random: Math.random() });
             console.log(newDoc, "Blog posted");
             toast.success("Blog posted successfuly!");
             localStorage.removeItem("current_post_article");
